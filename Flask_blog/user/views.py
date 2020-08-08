a895fd6522e7af878a1541ca4bc39597822518fd
nero2310 <nero2310@wp.pl>
2020-08-08 02:42:34 +0200

    Generate password hash using bcrypt

--- Flask_blog/user/views.py
+++ Flask_blog/user/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
+import bcrypt
 
 from Flask_blog.database.db import Mongo
 from Flask_blog.user.forms import UserRegisterForm
@@ -8,19 +9,28 @@ user = Blueprint("user", __name__)
 database = Mongo()
 
 
-@user.route("/user", methods=["GET", "POST"])
+def create_hash(password):
+    return str(bcrypt.hashpw(password.encode("utf-8"),bcrypt.gensalt(rounds=14)))
+
+
+@user.route("/register", methods=["GET", "POST"])
 def signup():
     form = UserRegisterForm(request.form)
     if form.validate() and request.method == "POST":
         user = {
             "username": form.username.data,
             "email": form.email.data,
-            "password": form.password.data,
+            "password_hash": create_hash(form.password.data)
         }
         database.insert(user)
-        flash(message="Your account has been registered")
+        flash(message=f"Your account has been registered {form.username.data}")
         return redirect(url_for("user.signup_sucess", _method="GET"))
     return render_template("auth/login_form.html", form=form)
+
+
+@user.route("/login",methods=["GET","POST"])
+# def log_in():
+#     # form =
 
 @user.route("/sucess",methods=["GET"])
 def signup_sucess():
