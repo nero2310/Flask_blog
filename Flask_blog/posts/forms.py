c3b0c6a09b596f0df09dea9bbbd46657e8649016
nero2310 <nero2310@wp.pl>
2020-08-22 23:51:01 +0200

    Title field now is unique

--- Flask_blog/posts/forms.py
+++ Flask_blog/posts/forms.py
@@ -5,6 +5,9 @@ class CreatePostForm(Form):
     title = StringField("Title", validators=[
         validators.Length(min=10, message="Title must have at least 10 chars"),
         validators.DataRequired()], render_kw={"placeholder": "Enter a post title"})
+    description = StringField("descrition",validators=[validators.Length(min="10",max="200",
+                                message="Your post description must be between 10 and 250 characters")],
+                              render_kw={"placeholder":"Enter a post description"})
 
     description = StringField("description", validators=[validators.Length(min=10,max=200,
                               message="Your post description must be between 10 and 250 characters"),
