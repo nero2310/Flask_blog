5380abdad33f7fdef0a426d115607de87e4c772d
nero2310 <nero2310@wp.pl>
2020-08-22 22:05:23 +0200

    Now post must contain field description

--- Flask_blog/posts/forms.py
+++ Flask_blog/posts/forms.py
@@ -6,8 +6,12 @@ class CreatePostForm(Form):
         validators.Length(min=10, message="Title must have at least 10 chars"),
         validators.DataRequired()], render_kw={"placeholder": "Enter a post title"})
 
+    description = StringField("description", validators=[validators.Length(min=10,max=200,
+                              message="Your post description must be between 10 and 250 characters"),
+                              validators.DataRequired()], render_kw={"placeholder":"Enter a post description"})
+
     content = StringField("content", validators=[
         validators.Length(min=10, max=100000, message="Your post must contain between 10 and 100000 characters"),
-        validators.DataRequired()], render_kw={"placeholder": "Enter a post content"})
+        validators.DataRequired()], render_kw={"placeholder": "Enter a post"})
 
     submit = SubmitField("Create Post")
--- Flask_blog/posts/views.py
+++ Flask_blog/posts/views.py
@@ -11,6 +11,7 @@ database = Mongo(database="test", collection="posts", ServerSelectionTimeoutMS=5
 @posts.route("/create", methods=["GET", "POST"])
 def create_post():
     form = forms.CreatePostForm(request.form)
+    print(form.validate())
     if form.validate() and request.method == "POST":
         if not "username" in session:
             flash("You have to be logged in to add a post")
@@ -18,23 +19,25 @@ def create_post():
             post = {
                 "title": form.title.data,
                 "content": form.content.data,
+                "description": form.description.data,
                 "approved": False,
                 "author": session["username"]
             }
             database.insert(post)
             flash("Post was created")
+            flash("Admin soon will check your post")
     return render_template("posts/create_post_form.html", form=form)
 
 
 @posts.route("/<title>")
 def get_post(title):
-    post = database.find(how_many="one", data_filter={"approved": True},
-                         projection={"content": 1, "title": 1, "author": 1})
-    return render_template("posts/render_post.html", post=post)
+    post = database.find(how_many="one",data_filter={"approved":True},
+                         projection={"content":1,"author":1,"title":1})
+    return render_template("posts/render_post.html",post = post)
 
 
 @posts.route("/", methods=["GET", "POST"])
 def list_post():
     all_post = database.find(how_many="all", data_filter={"approved": True},
-                             projection={"content": 1, "title": 1, "author": 1})
+                             projection={"content": 1, "title": 1, "description": 1, "author": 1})
     return render_template("posts/show_posts.html", posts=all_post)
