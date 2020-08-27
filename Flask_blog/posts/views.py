9c0df1c7759e8848fecba212d2bf25e41294527d
nero2310 <32717898+nero2310@users.noreply.github.com>
2020-08-28 01:57:14 +0200

    Features (#10)
    * Update README.MD
    
    * Click on the post title will redirect to page with post
    
    * Remove unnecessary print
    
    * Style changes
    
    * Simplifying post creating messages logic
    
    * Post now can render html if user is trusted
    
    If user/Post was trusted user can write his own html
    If user is untrusted user can only provide text (this should prevent
    from xss attack)
    
    * Move logout to auth,WIP admin page
    
    * Refactor login system

--- Flask_blog/posts/views.py
+++ Flask_blog/posts/views.py
@@ -6,14 +6,12 @@ from Flask_blog.posts import forms
 
 posts = Blueprint("posts", __name__)
 
-database = Mongo(database="test", collection="posts", ServerSelectionTimeoutMS=5000)
-
 
 @posts.route("/create", methods=["GET", "POST"])
 def create_post():
-    insert_sucess = True
+    database = Mongo(database="test", collection="posts", ServerSelectionTimeoutMS=5000)
+    insert_success = True
     form = forms.CreatePostForm(request.form)
-    print(form.validate())
     if form.validate() and request.method == "POST":
         if not "username" in session:
             flash("You have to be logged in to add a post")
@@ -23,28 +21,31 @@ def create_post():
                 "content": form.content.data,
                 "description": form.description.data,
                 "approved": False,
+                "xss_protection_disabled": False,
                 "author": session["username"]
             }
             try:
                 database.insert(post)
-            except DuplicateKeyError:
-                insert_sucess=False
-                flash("This post title exist")
-            if insert_sucess:
                 flash("Post was created")
                 flash("Admin soon will check your post")
+            except DuplicateKeyError:
+                flash("This post title exist")
     return render_template("posts/create_post_form.html", form=form)
 
 
 @posts.route("/<title>")
 def get_post(title):
-    post = database.find(how_many="one", data_filter={"approved": True},
-                         projection={"content": 1, "author": 1, "title": 1})
-    return render_template("posts/render_post.html", post=post)
+    database = Mongo(database="test", collection="posts", ServerSelectionTimeoutMS=5000)
+    post = database.find(how_many="one", data_filter={"approved": True, "title": title},
+                         projection={"content": 1, "author": 1, "title": 1, "xss_protection_disabled": 1})
+    database = Mongo(database="test", collection="user", ServerSelectionTimeoutMS=5000)
+    author = database.find(how_many="one", data_filter={"username": post["author"]}, projection={"trusted_user": 1})
+    return render_template("posts/render_post.html", post=post, author=author)
 
 
 @posts.route("/", methods=["GET", "POST"])
 def list_post():
+    database = Mongo(database="test", collection="posts", ServerSelectionTimeoutMS=5000)
     all_post = database.find(how_many="all", data_filter={"approved": True},
                              projection={"content": 1, "title": 1, "description": 1, "author": 1})
     return render_template("posts/show_posts.html", posts=all_post)
