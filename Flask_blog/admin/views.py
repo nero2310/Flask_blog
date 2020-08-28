2de652b4f271e8bfe6bd965d3e55e7faa85c687a
nero2310 <nero2310@wp.pl>
2020-08-28 16:17:30 +0200

    Working on displaying post properties in admin panel

--- Flask_blog/admin/views.py
+++ Flask_blog/admin/views.py
@@ -16,10 +16,20 @@ def admin_main():
         return "<h1>Somebody not logged in</h1>"
 
 
+@admin_page.route("/posts")
+def admin_posts():
+    if permissions_checker(session.get("username")):
+        database = Mongo(database="test", collection="posts")
+        posts = database.find(how_many="all", projection={"_id": 0, "title": 1, "approved": 1, "author": 1,
+                                                          "xss_protection_disabled": 1})
+        print(posts)
+        return render_template("admin/admin_posts.html", posts=posts)
+
+
 def permissions_checker(username) -> bool:
     """:return True if user is admin otherwise return False """
     database = Mongo(database="test", collection="user")
-    is_admin = database.find(how_many="one", data_filter={"username": session["username"]}, projection={"is_admin": 1})
+    is_admin = database.find(how_many="one", data_filter={"username": username}, projection={"is_admin": 1})
     if is_admin.get("is_admin"):
         return True
     return False
